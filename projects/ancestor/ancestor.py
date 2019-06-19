1cd16157320ef214299754ab0035886aa100ee95
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-19 09:13:49 +0200

    Complete ancestor tree

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,7 +1,18 @@
-"""
-Simple graph implementation
-"""
-from util import Stack, Queue
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
 
 
 class Graph:
@@ -10,151 +21,36 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex):
-        self.vertices[vertex] = set()
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        if v1 and v2 in self.vertices:
+        if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
         else:
-            raise IndexError("Vertex does not exist")
-
-    def bft(self, starting_vertex):
-        q = Queue()
-        q.enqueue(starting_vertex)
-        visited = set()
-        while q.size() > 0:
-            v = q.dequeue()
-            if v not in visited:
-                visited.add(v)
-                for next_node in self.vertices[v]:
-                    q.enqueue(next_node)
-
-    def dft(self, starting_vertex):
-        s = Stack()
-        s.push(starting_vertex)
-        visited = set()
-        while s.size() > 0:
-            v = s.pop()
-            if v not in visited:
-                visited.add(v)
-                for next_node in self.vertices[v]:
-                    s.push(next_node)
-
-    def dft_recursive(self, starting_vertex, visited=[]):
-        visited += [starting_vertex]
-
-        for next_node in self.vertices[starting_vertex]:
-            if next_node not in visited:
-                visited = self.dft_recursive(next_node, visited)
-
-        return visited
-
-    def bfs(self, starting_vertex, destination_vertex):
-        q = Queue()
-        q.enqueue([starting_vertex])
-        visited = set()
-        while q.size() > 0:
-            path = q.dequeue()
-            current = path[len(path) - 1]
-            if current not in visited:
-                if current == destination_vertex:
-                    return path
-                visited.add(current)
-                for next_node in self.vertices[current]:
-                    path_copy = path[:]
-                    path_copy.append(next_node)
-                    q.enqueue(path_copy)
-        return False
-
-    def dfs(self, starting_vertex, destination_vertex):
-        s = Stack()
-        s.push([starting_vertex])
-        visited = set()
-        while s.size() > 0:
-            path = s.pop()
-            current = path[len(path) - 1]
-            if current not in visited:
-                if current == destination_vertex:
-                    return path
-                visited.add(current)
-                for next_node in self.vertices[current]:
-                    path_copy = path[:]
-                    path_copy.append(next_node)
-                    s.push(path_copy)
+            raise IndexError("That vertex does not exist!")
 
 
-if __name__ == '__main__':
-    graph = Graph()  # Instantiate your graph
-    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
-    graph.add_vertex(1)
-    graph.add_vertex(2)
-    graph.add_vertex(3)
-    graph.add_vertex(4)
-    graph.add_vertex(5)
-    graph.add_vertex(6)
-    graph.add_vertex(7)
-    graph.add_edge(5, 3)
-    graph.add_edge(6, 3)
-    graph.add_edge(7, 1)
-    graph.add_edge(4, 7)
-    graph.add_edge(1, 2)
-    graph.add_edge(7, 6)
-    graph.add_edge(2, 4)
-    graph.add_edge(3, 5)
-    graph.add_edge(2, 3)
-    graph.add_edge(4, 6)
+def earliest_ancestor(ancestors, starting_node):
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        graph.add_edge(pair[1], pair[0])
 
-    '''
-    Should print:
-        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
-    '''
-    print(graph.vertices)
-
-    '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    graph.dft(1)
-
-    '''
-    Valid BFT paths:
-        1, 2, 3, 4, 5, 6, 7
-        1, 2, 3, 4, 5, 7, 6
-        1, 2, 3, 4, 6, 7, 5
-        1, 2, 3, 4, 6, 5, 7
-        1, 2, 3, 4, 7, 6, 5
-        1, 2, 3, 4, 7, 5, 6
-        1, 2, 4, 3, 5, 6, 7
-        1, 2, 4, 3, 5, 7, 6
-        1, 2, 4, 3, 6, 7, 5
-        1, 2, 4, 3, 6, 5, 7
-        1, 2, 4, 3, 7, 6, 5
-        1, 2, 4, 3, 7, 5, 6
-    '''
-graph.bft(1)
-
-'''
-    Valid DFT recursive paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-graph.dft_recursive(1)
-
-'''
-    Valid BFS path:
-        [1, 2, 4, 6]
-    '''
-print(graph.bfs(1, 6))
-
-'''
-    Valid DFS paths:
-        [1, 2, 4, 6]
-        [1, 2, 4, 7, 6]
-    '''
-print(graph.dfs(1, 6))
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        if(len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = path[:]
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliest_ancestor
